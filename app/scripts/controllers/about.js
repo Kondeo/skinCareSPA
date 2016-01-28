e4f9e5c5c30be91206863f93a3ed13fce80d198a
Julian Poyourow <julianpoyo@gmail.com>
2016-01-28 14:09:16 -0800

    Scaffolded

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name skinCareStaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the skinCareStaApp
+ */
+angular.module('skinCareStaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name skinCareStaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the skinCareStaApp
+ */
+angular.module('skinCareStaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
