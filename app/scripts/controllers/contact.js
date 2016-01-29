668f5f54053735a12f42992e1823c1e5de7ea016
Julian Poyourow <julianpoyo@gmail.com>
2016-01-28 17:14:09 -0800

    Massive updates

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name skinCareStaApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the skinCareStaApp
+ */
+angular.module('skinCareStaApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
