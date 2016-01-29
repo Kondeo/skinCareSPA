70cdb1a58c439d6df113804444f3609e37a4f89a
Julian Poyourow <julianpoyo@gmail.com>
2016-01-28 16:39:06 -0800

    Added data file and tile interface

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,21 @@
  * Controller of the skinCareStaApp
  */
 angular.module('skinCareStaApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.products = [];
+
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.open("get", "../data/products.json", true);
+    oReq.send();
+
+    function reqListener(e) {
+        $scope.products = JSON.parse(this.responseText);
+    }
   });
