f9ca178b8d68c402d58b76f06eb00a23286b8c51
Julian Poyourow <julianpoyo@gmail.com>
2016-01-31 11:57:15 -0800

    Added cart page

---
+++ app/scripts/controllers/cart.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name skinCareStaApp.controller:CartCtrl
+ * @description
+ * # CartCtrl
+ * Controller of the skinCareStaApp
+ */
+angular.module('skinCareStaApp')
+  .controller('CartCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
