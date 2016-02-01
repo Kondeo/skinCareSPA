5158c15be4227bbaeb4b7acfa2f9695136531b3e
Julian Poyourow <julianpoyo@gmail.com>
2016-01-31 20:47:30 -0800

    Added Paypal

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -39,4 +39,38 @@ angular.module('skinCareStaApp')
     }
 
     $scope.calcTotal();
+
+    $scope.checkoutPayPal = function (parms, clearCart) {
+
+      // global data
+      var data = {
+        cmd: "_cart",
+        business: parms.merchantID,
+        upload: "1",
+        rm: "2",
+        charset: "utf-8"
+      };
+
+      // item data
+      for (var i = 0; i < this.items.length; i++) {
+        var item = this.items[i];
+        var ctr = i + 1;
+        data["item_number_" + ctr] = item.sku;
+        data["item_name_" + ctr] = item.name;
+        data["quantity_" + ctr] = item.quantity;
+        data["amount_" + ctr] = item.price.toFixed(2);
+      }
+
+      // build form
+      var form = $('<form></form>');
+      form.attr("action", "https://www.paypal.com/cgi-bin/webscr");
+      form.attr("method", "POST");
+      form.attr("style", "display:none;");
+      this.addFormFields(form, data);
+      this.addFormFields(form, parms.options);
+      $("body").append(form);
+
+      form.submit();
+      form.remove();
+    }
   });
